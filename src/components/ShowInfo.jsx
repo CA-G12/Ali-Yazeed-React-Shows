85c429fd20c352ea232c9384cf02b0e212132a0a
ShbairAli <ali89freelancer@gmail.com>
2022-09-21 21:54:24 +0300

    fetched seasons and show's info within the details component relates #3

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -1,8 +1,9 @@
-const ShowInfo  = ({shows}) => {
-  return (
-      <div className="show-info">
-            show Info
-      </div>
+const ShowInfo = ({ info }) => {
+
+  return (info && 
+    <div className="show-info" key={info.id}>
+      <p> INFO: {info.summary.slice(3,-4)}</p>
+    </div>
   );
 }
 
--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,17 +1,18 @@
-const ShowList  = ({shows}) => {
+import { Link } from "react-router-dom";
 
-  console.log('INsid LIST COMP:', shows);
-  return shows.map((show => {
+const ShowList = ({ shows }) => {
+
+  return shows && shows.map((show => {
     const optimizedShow = show.show ? show.show : show;
     return (
-      < div className="show-card" >
-        <h2>NAME:{optimizedShow.name}</h2>
-        <p>RATING:{optimizedShow.rating.average}</p>
-        <p>ID:{optimizedShow.id}</p>
-        <img src={optimizedShow.image.medium} alt="" />
-
-
-      </ div>
+      <Link to={`/shows/${optimizedShow.id}`}>
+        < div className="show-card" key={optimizedShow.id}>
+          <h2>NAME:{optimizedShow.name}</h2>
+          <p>RATING:{optimizedShow.rating.average}</p>
+          <p>ID:{optimizedShow.id}</p>
+          <img src={optimizedShow.image.medium} alt="" />
+        </ div>
+      </Link>
       )
      
 }));
